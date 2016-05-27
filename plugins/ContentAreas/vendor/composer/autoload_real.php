0e21a26acea57229df1d722668f9a984e4d9e4b0
Duncan Cameron <phplist@dcameron.me.uk>
2016-05-27 18:43:24 +0100

    Regenerate composer autoloader

--- plugins/ContentAreas/vendor/composer/autoload_real.php
+++ plugins/ContentAreas/vendor/composer/autoload_real.php
@@ -19,26 +19,33 @@ class ComposerAutoloaderInitf33b3a17c07f7e612c9ad471b87aeaee
             return self::$loader;
         }
 
-        spl_autoload_register(array('ComposerAutoloaderInitf33b3a17c07f7e612c9ad471b87aeaee', 'loadClassLoader'), true, true);
+        spl_autoload_register(array('ComposerAutoloaderInitf33b3a17c07f7e612c9ad471b87aeaee', 'loadClassLoader'), true, false);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf33b3a17c07f7e612c9ad471b87aeaee', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInitf33b3a17c07f7e612c9ad471b87aeaee::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
-
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
-        }
-
-        $loader->register(true);
+        $loader->register(false);
 
         return $loader;
     }
