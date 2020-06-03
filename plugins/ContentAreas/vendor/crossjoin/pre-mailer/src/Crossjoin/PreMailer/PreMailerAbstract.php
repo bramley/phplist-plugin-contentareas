1f487e372b40e0d466af7ea70a2ab826176419c6
Duncan Cameron <phplist@dcameron.me.uk>
2020-06-03 11:42:33 +0100

    Update Premailer to be compatible with current version of symfony/css-selector

--- plugins/ContentAreas/vendor/crossjoin/pre-mailer/src/Crossjoin/PreMailer/PreMailerAbstract.php
+++ plugins/ContentAreas/vendor/crossjoin/pre-mailer/src/Crossjoin/PreMailer/PreMailerAbstract.php
@@ -9,7 +9,7 @@ use Crossjoin\Css\Format\Rule\Style\StyleRuleSet;
 use Crossjoin\Css\Format\Rule\Style\StyleSelector;
 use Crossjoin\Css\Reader\CssString;
 use Crossjoin\Css\Writer\WriterAbstract;
-use Symfony\Component\CssSelector\CssSelector;
+use Symfony\Component\CssSelector\CssSelectorConverter;
 
 abstract class PreMailerAbstract
 {
@@ -357,10 +357,12 @@ abstract class PreMailerAbstract
         }
 
         // Process all style declarations in the correct order
+        $cssSelectorConverter = new CssSelectorConverter();
+
         foreach ($specificityKeys as $specificityKey) {
             /** @var StyleDeclaration[] $declarations */
             foreach ($selectors[$specificityKey] as $selectorString => $declarations) {
-                $xpathQuery = CssSelector::toXPath($selectorString);
+                $xpathQuery = $cssSelectorConverter->toXPath($selectorString);
                 $elements = $xpath->query($xpathQuery);
                 /** @var \DOMElement $element */
                 foreach ($elements as $element) {
